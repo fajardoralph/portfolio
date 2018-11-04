7bb9313f6897eb2436a9adb6e0e6fa891781f115
unknown <fajardo.ralphangelo@gmail.com>
2018-11-05 06:39:02 +0800

    portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+$(function(){
+
+	$('.nav-toggle').on('mouseover',function(){
+		$('.wrapper').toggleClass('open');
+		
+		});
+
+	});
+
+
+$(function(){
+
+	$('.extra-toggle').on('click',function(){
+		$('.extra').toggleClass('open');
+		$('.extra-button').toggleClass('toggle');
+		});
+
+	});
+
+
+/*social-icons*/
+
+$(function(){
+
+	$('.social-icons').on('mouseover',function(){
+		$('.wrapper').toggleClass('open');
+		
+		});
+
+	});
+
+$(function(){
+
+	$('.social-icons').on('mouseout',function(){
+		$('.wrapper').toggleClass('open');
+		
+		});
+
+	});
